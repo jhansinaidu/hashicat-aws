790deecc12252d4308b4a46f97f1fc58e0a71003
jhansinaidu <jhansinaidupappu@gmail.com>
2021-02-10 20:42:15 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,11 @@ module "s3-bucket" {
   source  = "app.terraform.io/trainingHasicorp/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-}
 
-variable "bucket" {
+  variable "bucket" {
   description = "Name of S3 bucket to use"
   type        = string
   default     = ""
+}
+
 }
